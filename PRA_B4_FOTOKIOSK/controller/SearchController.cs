b8d6b478a1da9fc90e647d2e5c25aab231eae688
lucassss08_nl <lucaskoolen08@gmail.com>
2025-05-22 15:41:58 +0200

    b2 (voor hoever af zodat thomas hier verder kan)

--- PRA_B4_FOTOKIOSK/controller/SearchController.cs
+++ PRA_B4_FOTOKIOSK/controller/SearchController.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace PRA_B4_FOTOKIOSK.controller
 {
@@ -25,7 +26,76 @@ namespace PRA_B4_FOTOKIOSK.controller
         public void SearchButtonClick()
         {
 
+
+
+            if (Window.weekOfDay.SelectedItem is not ComboBoxItem selectedWeekOfDay)
+            {
+                return;
+            }
+
+            string selectedWeekOfDayString = selectedWeekOfDay.Content.ToString();
+            var now = DateTime.Now;
+            int today = (int)now.DayOfWeek;
+
+
+            // Initializeer de lijst met fotos
+            if (selectedWeekOfDayString == "zondag")
+            {
+                today = 0;
+            }
+            else if (selectedWeekOfDayString == "maandag")
+            {
+                today = 1;
+            }
+            else if (selectedWeekOfDayString == "dinsdag")
+            {
+                today = 2;
+            }
+            else if (selectedWeekOfDayString == "woensdag")
+            {
+                today = 3;
+            }
+            else if (selectedWeekOfDayString == "donderdag")
+            {
+                today = 4;
+            }
+            else if (selectedWeekOfDayString == "vrijdag")
+            {
+                today = 5;
+            }
+            else if (selectedWeekOfDayString == "zaterdag")
+            {
+                today = 6;
+            }
+
+            foreach (string dir in Directory.GetDirectories(@"../../../fotos"))
+            {
+                string folderName = Path.GetFileName(dir);
+                if (!folderName.StartsWith(today.ToString() + "_")) continue;
+
+                // Haal alle afbeeldingsbestanden op uit de juiste map
+                string[] imageFiles = Directory.GetFiles(dir, "*.*")
+                    .Where(file => file.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
+                                   file.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase) ||
+                                   file.EndsWith(".png", StringComparison.OrdinalIgnoreCase))
+                    .ToArray();
+
+                foreach (string imagePath in imageFiles)
+                {
+                    // Hier kun je iets doen met het pad van het beeld
+                    Console.WriteLine($"Gevonden foto: {imagePath}");
+
+                    // Als je metadata wil uitlezen:
+                    // var image = new FileInfo(imagePath);
+                    // var created = image.CreationTime;
+                }
+            }
+
         }
+
 
     }
 }
+
+    
+
