d4bca23327046c122b1b7228f424c15d7f849dc0
Croissantje1667 <d310843@edu.curio.nl>
2025-05-28 12:04:09 +0200

    b2 alerts

--- PRA_B4_FOTOKIOSK/controller/SearchController.cs
+++ PRA_B4_FOTOKIOSK/controller/SearchController.cs
@@ -27,85 +27,63 @@ namespace PRA_B4_FOTOKIOSK.controller
         // Wordt uitgevoerd wanneer er op de Zoeken knop is geklikt
         public void SearchButtonClick()
         {
-
             var now = DateTime.Now;
             int today = (int)now.DayOfWeek;
 
-            //if (Window.weekOfDay.SelectedItem is not ComboBoxItem selectedWeekOfDay)
-            //{
-            //    return;
-            //}
             string time = SearchManager.GetSearchInput();
-            //string time = Window.tbZoeken.Text;
-            //Window.lbSearchInfo.Content = text;
 
-            //var fotoDate = DateTime.Parse(photo.Source.Split("\\")[2].Split("_id")[0].Replace("_", ":"));
+            if (string.IsNullOrWhiteSpace(time))
+            {
+                MessageBox.Show("Voer een tijd in.");
+                return;
+            }
 
             string[] timeParts = time.Split(':');
-            if (int.TryParse(timeParts[0], out int iHour) &&
-                int.TryParse(timeParts[1], out int iMinute) &&
-                int.TryParse(timeParts[2], out int iSecond))
+            if (timeParts.Length != 3 ||
+                !int.TryParse(timeParts[0], out int iHour) ||
+                !int.TryParse(timeParts[1], out int iMinute) ||
+                !int.TryParse(timeParts[2], out int iSecond))
             {
-                if (timeParts == null || timeParts.Length != 3)
+                MessageBox.Show("Ongeldige invoer. Voer een geldige tijd in als hh:mm:ss.");
+                return;
+            }
+
+            bool isFound = false;
+
+            foreach (string dir in Directory.GetDirectories(@"../../../fotos"))
+            {
+                string folderName = Path.GetFileName(dir);
+                if (!folderName.StartsWith(today.ToString() + "_")) continue;
+
+                foreach (string file in Directory.GetFiles(dir))
                 {
-                    MessageBox.Show("Geen geldige invoer");
-                    return;
-                }
+                    string fileName = Path.GetFileName(file);
+                    string[] fileParts = fileName.Split('_');
 
-                bool isFound = false;
-
-
-                //string searchTerm = SearchManager.GetSearchInput()?.ToLower() ?? "";
-
-                foreach (string dir in Directory.GetDirectories(@"../../../fotos"))
-                {
-                    string folderName = Path.GetFileName(dir);
-                    if (!folderName.StartsWith(today.ToString() + "_")) continue;
-
-                    foreach (string file in Directory.GetFiles(dir))
+                    if (fileParts.Length > 3 &&
+                        int.TryParse(fileParts[0], out int hour) &&
+                        int.TryParse(fileParts[1], out int minute) &&
+                        int.TryParse(fileParts[2], out int second))
                     {
-                        string fileName = Path.GetFileName(file);
-                        string[] fileParts = fileName.Split('_');
-
-                        if (fileParts.Length > 3)
+                        if (hour == iHour && minute == iMinute && second == iSecond)
                         {
-                            if (int.TryParse(fileParts[0], out int hour) &&
-                                int.TryParse(fileParts[1], out int minute) &&
-                                int.TryParse(fileParts[2], out int second))
-                            {
-                                if (hour == iHour && minute == iMinute && second == iSecond)
-                                {
-                                    SearchManager.SetPicture(file);
-                                    isFound = true;
+                            SearchManager.SetPicture(file);
+                            isFound = true;
 
-                                    string displaySecond = second.ToString();
-                                    string displayMinute = minute.ToString();
+                            string displayMinute = minute.ToString("D2");
+                            string displaySecond = second.ToString("D2");
 
-                                    if (minute.ToString().Length == 1)
-                                    {
-                                        displayMinute = "0" + minute;
-
-                                    }
-
-                                    if (second.ToString().Length == 1)
-                                    {
-                                        displaySecond = "0" + second;
-                                    }
-
-                                    // Haal alle afbeeldingsbestanden op uit de juiste map
-
-                                    string text = "Foto gevonden:  - " + hour + ":" + displayMinute + ":" + displaySecond + " met id: " + fileParts[3].Substring(2, fileParts[3].Length - 6);
-                                    //SearchManager.SetSearchImageInfo(text);
-                                    Window.lbSearchInfo.Content = text;
-
-                                }
-
-                            }
-
-
+                            string text = $"Foto gevonden: - {hour}:{displayMinute}:{displaySecond} met id: {fileParts[3].Substring(2, fileParts[3].Length - 6)}";
+                            Window.lbSearchInfo.Content = text;
+                            return;
                         }
                     }
                 }
+            }
+
+            if (!isFound)
+            {
+                MessageBox.Show("Geen foto gevonden op dit tijdstip.");
             }
         }
     }
